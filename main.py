4adb0a69749236ba12097801d1c9abefd9c6173a
luismbacha <luismartinezbacha@hotmail.com>
2023-05-28 21:03:37 -0600

    PoC timer

---
+++ main.py
@@ -0,0 +1,7 @@
+import schedule_hw
+
+def main():
+    schedule_hw.start_hw()
+
+if __name__ == '__main__':
+    main()
---
+++ schedule_hw.py
@@ -0,0 +1,8 @@
+
+import hello_world
+import time
+
+def start_hw():
+    for x in range(1, 5):
+        hello_world.print_hello_world()
+        time.sleep(5)
