b2a60125106a6af5da9980a1a7dcfdad201a4902
luismbacha <luismartinezbacha@hotmail.com>
2023-07-23 12:34:06 -0600

    Created signal handler

--- start.py
+++ start.py
@@ -1,18 +1,21 @@
-from win11toast import toast
-from scheduler import Scheduler
+from scheduler import timer_loop
+from signal import signal, SIGINT
+from win32gui import EnumWindows, IsWindowVisible, GetWindowText, PostMessage
+from win32con import WM_CLOSE
 
-import sys
 import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--is-break", action="store_true", help="Flag to determine if it starts with a break block")
     args = parser.parse_args()
-    scheduler = Scheduler(args.is_break)
-    scheduler.timer_loop()
     def signal_handler(signum, frame):
         print("Received signal")
-        exit()
-    # signal.signal(signal.SIGINT, signal_handler)
+        for hwnd in EnumWindows():
+            if IsWindowVisible and GetWindowText.startswith("Toast Notification"):
+                PostMessage(hwnd, WM_CLOSE, 0, 0)
+        timer_loop(args.is_break)
+    signal(SIGINT, signal_handler)
+    timer_loop(args.is_break)
     
 main()
