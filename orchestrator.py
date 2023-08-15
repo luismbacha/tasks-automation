0007382807a19c93565366f36cd34e3282fd3140
luismbacha <luismartinezbacha@hotmail.com>
2023-08-15 07:20:23 -0600

    move logic out of scheduler

--- orchestrator.py
+++ orchestrator.py
@@ -1,44 +1,93 @@
 from trello_connection import get_trello_client
-
-BREAK = "Break"
-TOMATO = "Tomato"
+from notifier import Notifier
+from time import sleep
 
 class Orchestrator:
+    _instance = None
+        
+    def __new__(cls, start_as_break=False):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
     
-    def __init__(self, start_with_break) -> None:
-        if(start_with_break):
-            self.current_block = BREAK
-        else:
-            self.current_block = TOMATO
-        # self.trello_client = get_trello_client()
-        # Get boards and lists
-        pass
+    @staticmethod
+    def Initialize(start_as_break=False):
+        return Orchestrator(start_as_break)
+    
 
-    def orchestrate(self) -> dict:
+    def __init__(self, start_as_break) -> None:
+        if not hasattr(self, "_is_break"):
+            self._is_break = start_as_break
+            self._is_starting = True
+            self._block_type = { False: "Pomodoro", True:"Break" }
+            self._toast_values = {"title": "", "message":"Specify time, 0 to exit", "input": "time",
+                                "button": "OK", "dialogue": ""}
+            self._notify_values = {"title": "Block", "total": ""}
+            self._timer = -1
+            # self.trello_client = get_trello_client()
+            # Get boards and lists
+    
+    def _start_block(self) -> bool:
+        self._toast_values["title"] = self._block_type[self._is_break]
+        self._toast_values["dialogue"] = "Se termino el " + self._block_type[not self._is_break]
+        try:
+            self._timer = Notifier().notify(self._toast_values)
+            if(self._timer == -1):
+                print("Error loading time")
+                return True
+            if(self._timer == 0):
+                return False
+        except Exception as e:
+            print("There was an error during " + self._block_type[self._is_break] + " start")
+            return True
+        self._is_starting = not self._is_starting
+        return True
+    
+    def _finish_block(self) -> bool:
+        self._is_starting = not self._is_starting
+        self._notify_values["total"] = str(self._timer)
+        self._timer = -1
+        try:
+            Notifier().notify(self._notify_values)
+        except Exception as e:
+            print("There was an error during " + self._block_type[self._is_break] + " finish")
+            return True
+        self._is_break = not self._is_break
+        return True
+
+    def orchestrate(self) -> bool:
+        if(self._is_break and self._is_starting):
+            return self._start_break()
+        elif(self._is_break and not self._is_starting):
+            return self._finish_break()
+        elif(not self._is_break and self._is_starting):
+            return self._start_tomato()
+        elif(not self._is_break and not self._is_starting):
+            return self._finish_tomato()
         # Define if it's break, tomato or eod
         # Return values for notification
-        pass
+        return None
 
-    def start_break(self) -> None:
+    def _start_break(self) -> bool:
+        return self._start_block()
         # suggest break, move break list or create cards
-        pass
 
-    def finish_break(self) -> None:
-        # Remove breaks cards
-        pass
-
-    def start_tomato(self) -> None:
+    def _start_tomato(self) -> bool:
+        return self._start_block()
         # Move task to work on and multitasking task
         # Move "expired" cards to Doing
         # Ensure cards are correctly ordered
         # Show notification with tasks to work on
-        pass
 
-    def finish_tomato(self) -> None:
+    def _finish_break(self) -> bool:
+        return self._finish_block()
+        # Remove breaks cards
+
+    def _finish_tomato(self) -> bool:
+        return self._finish_block()
         # Move tasks from Doing to Today, Multitasking or Tasks
-        pass
 
-    def execute_eod(self) -> None:
+    def _execute_eod(self) -> None:
         # Show next familiar reminder
         # Arrange cards in Pending and Scheduled boards
         pass
--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,12 @@
-from notifier import Notifier
 from time import sleep
+from orchestrator import Orchestrator
 
-def timer_loop(is_break) -> None:
-    block_type = { False: "Pomodoro", True:"Break" }
-    toast_values = {"title": "", "message":"Specify time, 0 to exit", "input": "time",
-                    "button": "OK", "dialogue": ""}
-    notify_values = {"title": "Block", "total": ""}
-    while True:
+def timer_loop() -> None:
+    orchestrator = Orchestrator.Initialize()
+    repeat = True
+    while repeat:
         try:
-            toast_values["title"] = block_type[is_break]
-            toast_values["dialogue"] = "Se termino el " + block_type[not is_break]
-            timer = Notifier().toast(toast_values)
-            if(timer == 0):
-                return
-            is_break = not is_break
-            notify_values["total"] = str(timer)
-            Notifier().notify(notify_values)
+            repeat = orchestrator.orchestrate()
         except Exception as e:
             print("There was an error with timer loop")
             print(str(e))
